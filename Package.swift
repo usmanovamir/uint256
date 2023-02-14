e94762e34fe0a704b6c310c697ee2966233ecab2
Амир Усманов <amir@Amirs-MacBook-Pro.local>
2023-02-14 06:09:47 +0500

    UInt256 Swift Library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "uint256",
-            targets: ["AUInt256"]
+            targets: ["uint256"]
         ),
     ],
     targets: [
